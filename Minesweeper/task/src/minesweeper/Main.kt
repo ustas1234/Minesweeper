a4ba6e8cab3e6151b3320b23f3869da0ef5c9edc
Egor <ustas1234@gmail.com>
2020-07-23 12:25:11 +0300

    almost done todo fix unmarking * when free neighbor cell

--- Minesweeper/task/src/minesweeper/Main.kt
+++ Minesweeper/task/src/minesweeper/Main.kt
@@ -1,5 +1,7 @@
 package minesweeper
 
+import com.sun.org.apache.xpath.internal.functions.FuncFalse
+import com.sun.xml.internal.fastinfoset.util.StringArray
 import kotlin.random.Random
 
 
@@ -8,11 +10,13 @@ class MineFieldOperations {
         private lateinit var mineField: Array<CharArray>
         private lateinit var mineFieldToDraw: Array<CharArray>
         var gameState = "Starting"
+        var totalMines= 0
         var minesUnmarked = 0
+        var canAutoMark = false
 
         fun userMarks(uInputNumOfMines: Int) {
             print("Set/unset mines marks or claim a cell as free: > ")
-            val (userYInput, userXInput, userAction) = readLine()!!.split(' ')
+            val (userXInput, userYInput, userAction) = readLine()!!.split(' ')
 
             if (MineFieldOperations.gameState == "Starting" && userAction == "free") {
                 MineFieldOperations.create(9, 9, uInputNumOfMines,userXInput.toInt(),userYInput.toInt())
@@ -21,44 +25,59 @@ class MineFieldOperations {
 
             when (userAction) {
                 "free" -> {
-
+                    when (mineField[userYInput.toInt() - 1][userXInput.toInt() - 1]) {
+                        '*', '.', in '0'.. '9' -> {
+                            showHints (userXInput.toInt() - 1, userYInput.toInt() - 1)
+                            drawField()
+                        }
+                        'X' -> {
+                            println("You stepped on a mine and failed!")
+                            gameState = "End"
+                        }
+                    }
                 }
                 "mine" -> {
+                    when (mineFieldToDraw[userYInput.toInt() - 1][userXInput.toInt() - 1]) {
+                        '.' -> {
+                            mineFieldToDraw[userYInput.toInt() - 1][userXInput.toInt() - 1] = '*'
+                            drawField()
+                        }
+                        '*' -> {
+                            mineFieldToDraw[userYInput.toInt() - 1][userXInput.toInt() - 1] = '.'
+                            drawField()
+                        }
 
+                    }
                 }
+                "show" -> drawFieldReal()
             }
 
-            when (mineField[userXInput.toInt() - 1][userYInput.toInt() - 1]) {
-                in '0'..'9' -> println("There is a number here!")
-                'X' -> {
-                    mineField[userXInput.toInt() - 1][userYInput.toInt() - 1] = 'M'
-                    drawField()
-                    minesUnmarked--
-                }
-                '.' -> {
-                    mineField[userXInput.toInt() - 1][userYInput.toInt() - 1] = 'm'
-                    drawField()
-                    minesUnmarked++
-                }
-                'M' -> {
-                    mineField[userXInput.toInt() - 1][userYInput.toInt() - 1] = 'X'
-                    drawField()
-                    minesUnmarked++
-                }
-                'm' -> {
-                    mineField[userXInput.toInt() - 1][userYInput.toInt() - 1] = '.'
-                    drawField()
-                    minesUnmarked--
+            if (gameState == "Going") checkIfWin()
+
+
+
+
+        }
+
+        private fun checkIfWin (){
+            var isAllMinesChecked = true
+            var isNonMinesExplored = true
+            for (i in 0..8) {
+                for (j in 0..8) {
+                    if (mineField[i][j] == 'X') {
+                        if (mineFieldToDraw[i][j] != '*') isAllMinesChecked = false
+                    } else {
+                        if (mineFieldToDraw[i][j] == '*' || mineFieldToDraw[i][j] == '.') isNonMinesExplored = false
+                    }
                 }
             }
-
-            if (minesUnmarked == 0) {
-                gameState = "End"
+            if (isAllMinesChecked && isNonMinesExplored) {
                 println("Congratulations! You found all the mines!")
+                gameState = "End"
             }
         }
 
-        fun create(rowNumber: Int, columnNumber: Int, minesNumberinput: Int, firstCellX: Int,firstCellY: Int) {
+        private fun create(rowNumber: Int, columnNumber: Int, minesNumberinput: Int, firstCellX: Int, firstCellY: Int) {
             val minesNumber: Int
             var currentNumOfMines = 0
             var randomRow: Int
@@ -80,7 +99,8 @@ class MineFieldOperations {
             while (currentNumOfMines != minesNumber) {
                 randomRow = Random.nextInt(0, rowNumber)
                 randomColumn = Random.nextInt(0, columnNumber)
-                if (mineField[randomRow][randomColumn] == '.'&& (randomColumn != firstCellX && randomRow != firstCellY)) {
+                if (mineField[randomRow][randomColumn] == '.') {
+                    if (randomColumn == firstCellX - 1 && randomRow == firstCellY - 1) continue
                     mineField[randomRow][randomColumn] = 'X'
                     currentNumOfMines++
                 }
@@ -90,7 +110,7 @@ class MineFieldOperations {
 
         }
 
-        fun createToDraw (rowNumber: Int, columnNumber: Int) {
+        fun createToDraw (columnNumber: Int, rowNumber: Int) {
             for (i in 0..rowNumber) {
                 for (j in 0..columnNumber) {
                     mineFieldToDraw = Array(i) { CharArray(j) { '.' } }
@@ -104,11 +124,22 @@ class MineFieldOperations {
             for (i in mineFieldToDraw.indices) {
                 print("${i + 1}|")
                 for (j in mineFieldToDraw[i].indices) {
-                    when (mineFieldToDraw[i][j]) {
-                        'X' -> print('.')
-                        'm','M' -> print('*')
-                        else -> print(mineFieldToDraw[i][j])
-                    }
+                    print(mineFieldToDraw[i][j])
+                }
+                print("|\n")
+
+
+            }
+            println("—│—————————│")
+        }
+
+        private fun drawFieldReal() {
+            println(" │123456789│\n" +
+                    "—│—————————│")
+            for (i in mineField.indices) {
+                print("${i + 1}|")
+                for (j in mineField[i].indices) {
+                    print(mineField[i][j])
                 }
                 print("|\n")
 
@@ -119,6 +150,90 @@ class MineFieldOperations {
 
         private fun isMine(rowNumber: Int, columnNumber: Int): Boolean {
             return mineField[rowNumber][columnNumber] == 'X'
+        }
+
+        private  fun showHints (X: Int, Y: Int){
+            if (mineField[Y][X] !in '0'..'9') floodHints(X,Y, mineField[Y][X]) else {
+                            for (i in -1..1) {
+                for (j in -1..1){
+                    if ((X + j) in 0..8 && (Y + i) in 0..8) {
+                        when (mineField[Y + i][X + j]) {
+                            '.' -> mineFieldToDraw[Y + i][X + j] = '/'
+                            '*' -> mineFieldToDraw[Y + i][X + j] = '/'
+                            'X' -> mineFieldToDraw[Y + i][X + j] = '.'
+                            else -> {
+                                mineFieldToDraw[Y + i][X + j] = mineField[Y + i][X + j]
+                            }
+                        }
+
+/*                            if (!((i == 0) && (j == 0)) &&
+                                    ((mineFieldToDraw[Y + i][X + j] !in '0'..'9') &&
+                                            (mineField[Y + i][X + j] != 'X'))) {
+                                println("Found to go: X${X+ j} Y${Y + i}")
+                                //showHints(X + j, Y + i)
+                            }*/
+
+                    }
+                }
+            }
+            }
+
+
+
+
+        }
+
+        private fun floodHints(X: Int, Y: Int, prevValue: Char, nextValue: Char = '/'){
+            if ((X !in 0..8) || (Y !in 0..8)) return
+       //   if (prevValue != mineField[Y][X] || mineFieldToDraw[Y][X] == '/') return
+            if (prevValue != mineField[Y][X]) return
+         //   if (mineField[Y][X] in '0'..'9') return
+
+            mineField[Y][X] = nextValue
+            mineFieldToDraw[Y][X] = nextValue
+
+            for (i in -1..1) {
+                for (j in -1..1){
+                    if ((X + j) in 0..8 && (Y + i) in 0..8) {
+                        when (mineField[Y + i][X + j]) {
+                            '.' -> mineFieldToDraw[Y + i][X + j] = '/'
+                            '*' -> mineFieldToDraw[Y + i][X + j] = '/'
+                            'X' -> mineFieldToDraw[Y + i][X + j] = '.'
+                            else -> {
+                                mineFieldToDraw[Y + i][X + j] = mineField[Y + i][X + j]
+                            }
+                        }
+
+                        /*if (!((i == 0) && (j == 0)) &&
+                                ((mineFieldToDraw[Y + i][X + j] !in '0'..'9') &&
+                                        (mineField[Y + i][X + j] != 'X'))) {
+                            println("Found to go: X${X+ j} Y${Y + i}")
+                            //showHints(X + j, Y + i)
+                        }*/
+
+                    }
+                }
+            }
+
+
+            floodHints(X + 1, Y, prevValue, nextValue)
+            floodHints(X - 1, Y, prevValue, nextValue)
+            floodHints(X , Y + 1, prevValue, nextValue)
+            floodHints(X , Y - 1, prevValue, nextValue)
+
+        }
+
+
+
+        private fun checkNeighbor(X: Int, Y: Int) {
+            for (i in -1..1) {
+                for (j in -1..1) {
+                    if ((X + j) in 0..8 && (Y + i) in 0..8) {
+                        if (mineField[Y + i][X + j] == '.') canAutoMark = true
+                    }
+
+                }
+            }
         }
 
         private fun fillWithHints() {
@@ -216,7 +331,6 @@ class MineFieldOperations {
                     }
                 }
             }
-
         }
     }
 }
@@ -228,9 +342,7 @@ fun main() {
     val uInputNumOfMines = readLine()!!.toInt()
     MineFieldOperations.createToDraw(9,9)
     MineFieldOperations.drawField()
-
-
-
+    MineFieldOperations.totalMines = uInputNumOfMines
 
   //  MineFieldOperations.create(9,9, uInputNumOfMines)
     while (MineFieldOperations.gameState != "End"){
